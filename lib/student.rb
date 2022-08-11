34652d0dc43a20989d71ca2954af8a7a9edfcf2a
MarkXtenda <mark.i2p.1998@gmail.com>
2022-08-11 13:52:12 +1000

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
 
+    attr_reader :first_name, :last_name, :knowledge
+
+    def initialize
+        super
+        @knowledge = []
+    end
+
+    def learn(arg)
+        knowledge << arg
+    end
 end
